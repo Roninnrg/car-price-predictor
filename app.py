fec72481f4b65a26e9fc3a01f3e499e3dda0cec4
Roninnrg <romastar0104@gmail.com>
2026-06-14 20:10:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("Оценка стоимости автомобиля")
 st.write("Узнайте рыночную стоимость вашего авто с помощью ИИ (Цены указаны в индийских лакхах: 1 лакх ≈ 100 000 ₹)")
 
 with st.form("Подать заявку"):
-    # Передаем целые числа в value и step=1, чтобы убрать отображение десятых долей .00
+    
     Year = st.number_input("Год выпуска", min_value=2000, max_value=2026, value=2015, step=1)
     Present_Price = st.number_input("Цена новой машины в салоне (в лакхах, например: 5.59)", min_value=0.1, max_value=100.0, value=5.59, step=0.1)
     Kms_Driven = st.number_input("Пробег в километрах", min_value=0, max_value=500000, value=25000, step=1000)
@@ -35,5 +35,4 @@ if submit:
     response = requests.post("http://127.0.0.1:8000/car", json=data)
     predicted_price = response.json()["predicted_price"]
     
-    # Выводим результат с указанием валюты датасета
     st.success(f"Рекомендуемая цена продажи вашего автомобиля: {predicted_price:.2f} лакх (≈ {predicted_price * 100:.0f} 000 рупий)")
